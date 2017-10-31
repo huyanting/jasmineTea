22719ebcd541381759d41cdd7a90ac451cd037c2
Yanting.Hu <yanting.hu@nio.com>
2017-10-31 18:36:20 +0800

    config

---
+++ app/server/express_middlewares/template_middleware.js
@@ -0,0 +1,30 @@
+/* ************************************************************************
+*  <copyright file="template_middleware.js" hyting>
+*  Copyright (c) 2010, 2016 All Right Reserved, http://www.yantinghu.com
+*
+*  THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+*  KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+*  IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+*  PARTICULAR PURPOSE.
+*  </copyright>
+*  ***********************************************************************/
+
+const util = require('util');
+const utils = require('../utils');
+const vendor = require('../vendor');
+const version = require('../../version.json').version;
+const backgroudImage = '/dist/imgs/bg.jpg';
+
+exports.register = (app, serverConfigs, express) => {
+    app.locals.version = version;
+
+    app.get('/index', (request, response) => {
+        response.render('index', {
+            locals: {
+                previewClass: 'backgroundImg'
+            },
+            backgroudImage: backgroudImage,
+            viewName: 'app'
+        });
+    });
+};
