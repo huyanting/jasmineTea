71ba3156366537a6acd863821d0f65b95463525a
Yanting.Hu <yanting.hu@nio.com>
2017-11-13 19:47:56 +0800

    Add bootstrap

--- app/assets/pages/app.js
+++ app/assets/pages/app.js
@@ -9,3 +9,8 @@
 *  </copyright>
 *  ***********************************************************************/
 
+require('bootstrap');
+require('bootstrap/dist/css/bootstrap.css');
+
+require('../core/$core.scss');
+require('app.scss');
